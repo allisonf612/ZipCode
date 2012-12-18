e1ff56dc85d891af3dedde846f30450feaa9b5e6
Allison <lightning@Allisons-MacBook-Pro.local>
2012-12-18 14:07:52 -0600

    State integration tests passing.

---
+++ test/integration/allison/zipcode/StateIntegrationTests.groovy
@@ -0,0 +1,126 @@
+package allison.zipcode
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class StateIntegrationTests {
+    def unitedStates
+
+    @Before
+    void setUp() {
+        unitedStates = Country.findByName("United States of America")
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testValidState() {
+        def minnesota = new State(totalResultsCount: 1,
+                abbreviation: "MN",
+                fullName: "Minnesota")
+
+        unitedStates = Country.findByName("United States of America")
+        unitedStates.addToStates(minnesota)
+        assertNotNull unitedStates.save(flush: true)
+
+        def foundMinnesota = State.get(minnesota.id)
+        assertNotNull foundMinnesota
+
+        assertEquals 1, foundMinnesota.totalResultsCount
+        assertEquals "MN", foundMinnesota.abbreviation
+        assertEquals "Minnesota", foundMinnesota.fullName
+    }
+
+    @Test
+    void testHasMany() {
+        def minnesota = new State(totalResultsCount: 1,
+                abbreviation: "MN",
+                fullName: "Minnesota")
+
+        unitedStates = Country.findByName("United States of America")
+        unitedStates.addToStates(minnesota)
+        unitedStates.save(flush: true)
+
+        def zipcode1 = new Zipcode(postalCode: "55082",
+                name: "Stillwater",
+                countryCode: "US",
+                lat: 45.06142,
+                lng: -92.84736,
+                adminCode1: "MN",
+                adminName1: "Minnesota",
+                adminCode2: "163",
+                adminName2: "Washington")
+
+        def zipcode2 = new Zipcode(postalCode: "55044",
+                name: "Lakeville",
+                countryCode: "US",
+                lat: 44.67486,
+                lng: -93.2578,
+                adminCode1: "MN",
+                adminName1: "Minnesota",
+                adminCode2: "037",
+                adminName2: "Dakota")
+
+        minnesota.addToZipcodes(zipcode1)
+        minnesota.addToZipcodes(zipcode2)
+        minnesota.save(flush: true)
+
+        assertEquals 2, minnesota.zipcodes.size()
+
+        // Verify that
+        assertNotNull Zipcode.get(zipcode1.id)
+    }
+
+
+    @Test
+    void testCascadeDelete() {
+        def minnesota = new State(totalResultsCount: 1,
+                abbreviation: "MN",
+                fullName: "Minnesota")
+
+        unitedStates = Country.findByName("United States of America")
+        unitedStates.addToStates(minnesota)
+        unitedStates.save(flush: true)
+
+        def zipcode1 = new Zipcode(postalCode: "55082",
+                name: "Stillwater",
+                countryCode: "US",
+                lat: 45.06142,
+                lng: -92.84736,
+                adminCode1: "MN",
+                adminName1: "Minnesota",
+                adminCode2: "163",
+                adminName2: "Washington")
+
+        def zipcode2 = new Zipcode(postalCode: "55044",
+                name: "Lakeville",
+                countryCode: "US",
+                lat: 44.67486,
+                lng: -93.2578,
+                adminCode1: "MN",
+                adminName1: "Minnesota",
+                adminCode2: "037",
+                adminName2: "Dakota")
+
+        minnesota.addToZipcodes(zipcode1)
+        minnesota.addToZipcodes(zipcode2)
+        minnesota.save(flush: true)
+
+        assertEquals 2, minnesota.zipcodes.size()
+
+        // Verify that the zipcodes are there before delete
+        assertNotNull Zipcode.get(zipcode1.id)
+        assertNotNull Zipcode.get(zipcode2.id)
+
+        minnesota.country.removeFromStates(minnesota)
+        minnesota.delete()
+
+        // The delete was cascaded to the zipcodes
+        assertFalse State.exists(minnesota.id)
+        assertFalse Zipcode.exists(zipcode1.id)
+        assertFalse Zipcode.exists(zipcode2.id)
+    }
+}
