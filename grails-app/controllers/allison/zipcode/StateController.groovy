a77c6f2a89ce43fe8eb30726ade30af2b206f450
Allison <lightning@Allisons-MacBook-Pro.local>
2012-12-19 16:40:43 -0600

    Custom show action to links coming from the tag cloud.

--- grails-app/controllers/allison/zipcode/StateController.groovy
+++ grails-app/controllers/allison/zipcode/StateController.groovy
@@ -5,4 +5,30 @@ import org.springframework.dao.DataIntegrityViolationException
 class StateController {
     static scaffold = true
 
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [stateInstanceList: State.list(params), stateInstanceTotal: State.count()]
+    }
+
+
+    def show(Long id) {
+        def stateInstance
+        if (params.stateName) {
+            stateInstance = ZipcodeService.getState(Country.get(id), params.stateName)
+        } else {
+            stateInstance = State.get(id)
+        }
+        if (!stateInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'state.label', default: 'State'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [stateInstance: stateInstance]
+    }
+
 }
