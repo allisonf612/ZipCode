c4ee6c73d557f4e58bf9c97715db1a85e4200883
Allison <lightning@Allisons-MacBook-Pro.local>
2012-12-19 13:07:59 -0600

    ZipcodeService passes integration tests for getState and clearZipcodes

--- grails-app/services/allison/zipcode/DownloadService.groovy
+++ grails-app/services/allison/zipcode/DownloadService.groovy
@@ -1,5 +1,9 @@
 package allison.zipcode
 
+class UnableToDownloadException extends RuntimeException {
+    String message
+}
+
 class DownloadService {
 
     /**
@@ -8,22 +12,46 @@ class DownloadService {
     String download(String filename, String address)
     {
         File file = new File(filename)
-        file.getParentFile().mkdirs()
-        file.createNewFile()
+        print "File: " + file
+//        if (!file.getParentFile().mkdirs()) {
+//            println "Dirs not made"
+//        }
+
+        if (!file.createNewFile()) {
+            println "File not created"
+        } else {
+            println "File created"
+        }
+
         def fileOut = new FileOutputStream(file)
         def out = new BufferedOutputStream(fileOut)
         try {
             out << new URL(address).openStream()
         } catch (UnknownHostException ex) {
             // Cleanup
+            println "Deleting file"
             file.delete()
 
-            return "Unable to download from url: ${address}"
-        }
-        out.close()
+            throw new UnableToDownloadException(message: "Unable to download from url: ${address}")
+        } finally {
+            out.close()
 
-        return "Successfully downloaded zip codes"
+        }
+
     }
 
+    /**
+     * Return the name of the file for download. Returns temp
+     * if country is null.
+     * @param country
+     * @return
+     */
+    static String getCountryFileName(Country country) {
+        def prefix = "web-app/data/"
+        if (country?.countryCode) {
+            return "${prefix}${country.countryCode}"
+        }
+        return "${prefix}temp"
+    }
 
 }
